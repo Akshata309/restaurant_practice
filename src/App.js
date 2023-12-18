7b7054e016748e70efa78828bcacf283a0ed0221
Akshata309 <akshatakhadake309@gmail.com>
2023-12-18 14:15:57 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+//import UseState from "./Hooks/useState";
+//import UseEffect from "./Hooks/useEffect";
+//import UseReducer from "./Hooks/useReducer";
+import Restaurant from "./Basics/Restaurant";
+
+const App = () => {
+  return <Restaurant/>
+};
+
+export default App;
